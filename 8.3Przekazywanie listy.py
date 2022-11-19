abeb961efa126dce924d3d547525f2f0272ca425
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2022-11-19 22:19:03 +0100

    Commit

--- 8.3Przekazywanie listy.py
+++ 8.3Przekazywanie listy.py
@@ -50,3 +50,43 @@
 
 # nazwa_funkcji(nazwa_listy[:])
 # print_models(unprinted_designs[:],completed_models)
+
+
+#8.9
+
+# magicians=['MagicznyAndrzej','CzarownicaMonika','Paprodziad']
+# def show_magicians(names):
+#     """Pokazuje po kolei imiona z listy"""
+#     for name in magicians:
+#         print(name)
+# show_magicians(magicians)
+
+#8.10
+# def show_magicians(magicians):
+#     """Pokazuje po kolei imiona z listy"""
+#     for magician in magicians:
+#         print(magician)
+# def make_great(magicians):
+#     new_list=[]
+#     for magician in magicians:
+#         new_list.append("Doskonały " +magician.title()  )
+#     return new_list
+# magicians=['MagicznyAndrzej','CzarownicaMonika','Paprodziad']
+# new_list=make_great(magicians[:])
+# show_magicians(magicians)
+# show_magicians(new_list)
+#8.11
+
+def show_magicians(magicians):
+    """Pokazuje po kolei imiona z listy"""
+    for magician in magicians:
+        print(magician)
+def make_great(magicians):
+    new_list=[]
+    for magician in magicians:
+        new_list.append("Doskonały " +magician.title()  )
+    return new_list
+magicians=['MagicznyAndrzej','CzarownicaMonika','Paprodziad']
+new_list=make_great(magicians[:])
+show_magicians(magicians)
+show_magicians(new_list)
