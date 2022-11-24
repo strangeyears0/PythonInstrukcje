ba7697bfd09e2b9abffcb82375bc7dd1bff088f0
strangeyears0 <103994329+strangeyears0@users.noreply.github.com>
2022-11-24 21:28:18 +0100

    Commit

--- 9.1 Praca z klasami i egzemplarzami.py
+++ 9.1 Praca z klasami i egzemplarzami.py
@@ -1,17 +1,154 @@
 #KLASA CAR
 
-class Car():
-    """prosta próba zaprezentowania samochodu"""
+# class Car():
+#     """prosta próba zaprezentowania samochodu"""
+#
+#     def __init__(self,make, model, year):
+#         """Inicjalizacja atrybutów opisujących samochód"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#
+#     def get_descriptive_name(self):
+#         "Zwrot elegancko sformatowanego opisu samochodu"
+#         long_name = str(self.year) + '  ' + self.make + ' ' + self.model
+#         return long_name
+# car= Car("Reanault","Scenic",2000)
+# print(car.get_descriptive_name())
 
-    def __init__(self,make, model, year):
-        """Inicjalizacja atrybutów opisujących samochód"""
-        self.make = make
-        self.model = model
-        self.year = year
+#PRZYPISANIE ATRYBUTOWI WARTOŚCI DOMYŚLNEJ
 
-    def get_descriptive_name(self):
-        "Zwrot elegancko sformatowanego opisu samochodu"
-        long_name = str(self.year) + '  ' + self.make + ' ' + self.model
-        return long_name
-car= Car("Reanault","Scenic",2000)
-print(car.get_descriptive_name())
+# class Car():
+#     """prosta próba zaprezentowania samochodu"""
+#
+#     def __init__(self,make, model, year):
+#         """Inicjalizacja atrybutów opisujących samochód"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#
+#     def get_descriptive_name(self):
+#         "Zwrot elegancko sformatowanego opisu samochodu"
+#         long_name = str(self.year) + '  ' + self.make + ' ' + self.model
+#         return long_name
+#
+#     def read_odometer(self):
+#         """wyświetla informacje o przebiegu samochodu"""
+#         print(f"Ten samochód ma przejechane {str(self.odometer_reading)} km")
+#
+# car= Car("Reanault","Scenic",2000)
+# print(car.get_descriptive_name())
+# print(car.read_odometer())
+
+
+#MODYFIKACJA WARTOSCI ATRYBUTU :
+
+#BEZPOŚREDNIA MODYFIKACJA WARTOŚCI ATRYBUTU
+
+# class Car():
+#     """prosta próba zaprezentowania samochodu"""
+#
+#     def __init__(self, make, model, year):
+#         """Inicjalizacja atrybutów opisujących samochód"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#
+#     def get_descriptive_name(self):
+#         "Zwrot elegancko sformatowanego opisu samochodu"
+#         long_name = str(self.year) + '  ' + self.make + ' ' + self.model
+#         return long_name
+#
+#     def read_odometer(self):
+#         """wyświetla informacje o przebiegu samochodu"""
+#         print(f"Ten samochód ma przejechane {str(self.odometer_reading)} km")
+#
+#
+# car = Car("Reanault", "Scenic", 2000)
+# print(car.get_descriptive_name())
+# print(car.read_odometer())
+#
+# car.odometer_reading = 23
+# print(car.read_odometer())
+#
+
+#MODYFIKACJA WARTOŚCI ATRYBUTU ZA POMOCĄ METODY
+# class Car():
+#     """prosta próba zaprezentowania samochodu"""
+#
+#     def __init__(self, make, model, year):
+#         """Inicjalizacja atrybutów opisujących samochód"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 25
+#
+#     def get_descriptive_name(self):
+#         "Zwrot elegancko sformatowanego opisu samochodu"
+#         long_name = str(self.year) + '  ' + self.make + ' ' + self.model
+#         return long_name
+#
+#     def read_odometer(self):
+#         """wyświetla informacje o przebiegu samochodu"""
+#         print(f"Ten samochód ma przejechane {str(self.odometer_reading)} km")
+#
+#     def update_odometer(self, mileage):
+#         """Przypisanie podanej wartości licznikowi przebiegu"""
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("Nie mozna cofnąc licznika przebiegu ! ")
+#
+# car = Car("Reanault", "Scenic", 2000)
+# print(car.get_descriptive_name())
+#
+#
+# car.update_odometer(23)
+# car.read_odometer()
+
+
+#INKREMENTACJA WARTOŚCI ATRYBUTU ZA POMOCĄ METODY
+
+
+# class Car():
+#     """prosta próba zaprezentowania samochodu"""
+#
+#     def __init__(self, make, model, year):
+#         """Inicjalizacja atrybutów opisujących samochód"""
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#
+#     def get_descriptive_name(self):
+#         "Zwrot elegancko sformatowanego opisu samochodu"
+#         long_name = str(self.year) + '  ' + self.make + ' ' + self.model
+#         return long_name
+#
+#     def read_odometer(self):
+#         """wyświetla informacje o przebiegu samochodu"""
+#         print(f"Ten samochód ma przejechane {str(self.odometer_reading)} km")
+#
+#     def update_odometer(self, mileage):
+#         """Przypisanie podanej wartości licznikowi przebiegu"""
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("Nie mozna cofnąc licznika przebiegu ! ")
+#
+#     def increment_odometer(self, kilometers):
+#         """Inkrementacja wartosci licznika przebiegu o podana wartość"""
+#         self.odometer_reading += kilometers
+#
+# car = Car("Reanault", "Scenic", 2000)
+# print(car.get_descriptive_name())
+# car.update_odometer(270000)
+# car.read_odometer()
+#
+# car.increment_odometer(1000)
+# car.read_odometer()
+#
+# car.update_odometer(23)
+# car.read_odometer()
